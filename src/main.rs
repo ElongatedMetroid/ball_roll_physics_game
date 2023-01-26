610eb3361f2551128b515339873c2ee7ade5ab68
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-01-25 16:36:00 -0800

    add debug inspector

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier2d::{
     prelude::{NoUserData, RapierPhysicsPlugin},
     render::RapierDebugRenderPlugin,
@@ -6,12 +7,18 @@ use bevy_rapier2d::{
 use platformer::{camera::PlayerCameraPlugin, level::LevelPlugin, player::PlayerPlugin};
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(PlayerPlugin)
         .add_plugin(PlayerCameraPlugin)
-        .add_plugin(LevelPlugin)
-        .run();
+        .add_plugin(LevelPlugin);
+
+    if cfg!(debug_assertions) {
+        app.add_plugin(WorldInspectorPlugin)
+            .add_plugin(RapierDebugRenderPlugin::default());
+    }
+
+    app.run()
 }
